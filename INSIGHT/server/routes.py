0eff38f2e8c3aed23d166b210fe38640ea00c6d6
Cuong Nguyen <nguyentricuong1995@gmail.com>
2016-09-04 17:53:29 -0400

    Added boilerplate for a few API endpoints

--- INSIGHT/server/routes.py
+++ INSIGHT/server/routes.py
@@ -1,6 +1,44 @@
+import random
 from server import app
 from flask import render_template
+from flask import request
+from flask import jsonify
 
 @app.route('/')
-def hello_world():
+def index():
   return render_template('layout.html')
+
+
+@app.route('/query/random/')
+def api_random_query():
+    dataset_id = request.args.get('dataset', None)
+    query_len = request.args.get('length', 20)
+    if dataset_id:
+        # TODO(Cuong) randomly sample a query 
+        pass
+    else:
+        # TODO(Cuong) provide more parameters such as scaling, distribution...
+        query = [random.random() for count in range(query_len)]
+        return jsonify(query=query)
+
+
+@app.route('/query/sample/')
+def api_sample_query():
+    dataset_id = request.args.get('dataset', None)
+    # TODO(Cuong) other parameters (start, length, ...)
+    if dataset_id: 
+        # TODO(Cuong) pick the specified query 
+        pass
+    else:
+        # TODO(Cuong) response with error
+        pass
+
+
+@app.route('/find')
+    dataset_id = request.args.get('dataset', None)
+    if dataset_id:
+        pass
+    else:
+        pass
+
+
