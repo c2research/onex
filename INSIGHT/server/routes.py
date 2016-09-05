2c0fdf4f8fdb6f3cc5898c6a37b414018756f7a9
Cuong Nguyen <nguyentricuong1995@gmail.com>
2016-09-04 23:54:41 -0400

    Fixed indentation

--- INSIGHT/server/routes.py
+++ INSIGHT/server/routes.py
@@ -11,35 +11,35 @@ def index():
 
 @app.route('/query/random/')
 def api_random_query():
-    dataset_id = request.args.get('dataset', None)
-    query_len = request.args.get('length', 20)
-    if dataset_id:
-        # TODO(Cuong) randomly sample a query 
-        pass
-    else:
-        # TODO(Cuong) provide more parameters such as scaling, distribution...
-        query = [random.random() for count in range(query_len)]
-        return jsonify(query=query)
+  dataset_id = request.args.get('dataset', None)
+  query_len = request.args.get('length', 20)
+  if dataset_id:
+    # TODO(Cuong) randomly sample a query 
+    pass
+  else:
+    # TODO(Cuong) provide more parameters such as scaling, distribution...
+    query = [random.random() for count in range(query_len)]
+    return jsonify(query=query)
 
 
 @app.route('/query/sample/')
 def api_sample_query():
-    dataset_id = request.args.get('dataset', None)
-    # TODO(Cuong) other parameters (start, length, ...)
-    if dataset_id: 
-        # TODO(Cuong) pick the specified query 
-        pass
-    else:
-        # TODO(Cuong) response with error
-        pass
+  dataset_id = request.args.get('dataset', None)
+  # TODO(Cuong) other parameters (start, length, ...)
+  if dataset_id: 
+    # TODO(Cuong) pick the specified query 
+    pass
+  else:
+    # TODO(Cuong) response with error
+    pass
 
 
 @app.route('/find')
 def api_find_best_match():
-    dataset_id = request.args.get('dataset', None)
-    if dataset_id:
-        pass
-    else:
-        pass
+  dataset_id = request.args.get('dataset', None)
+  if dataset_id:
+    pass
+  else:
+    pass
 
 
