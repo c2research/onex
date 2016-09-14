1639fae9ca946aa5c3abea345b4b2155b73e4de4
cjlovering <cjlovering@130.215.14.76>
2016-09-14 01:06:58 -0400

    slight formatting of chart and hiding the flying legend

--- INSIGHT/app/js/components/InsightView.js
+++ INSIGHT/app/js/components/InsightView.js
@@ -7,6 +7,14 @@ var Chart = require('react-d3-core').Chart;
 // require `react-d3-basic` for Line chart component.
 var LineChart = require('react-d3-basic').LineChart;
 
+var Legend = require('react-d3-core').Legend;
+var Xaxis = require('react-d3-core').Xaxis;
+var Yaxis = require('react-d3-core').Yaxis;
+var Line = require('react-d3-basic').Line;
+var Xgrid = require('react-d3-core').Xgrid;
+var Ygrid = require('react-d3-core').Ygrid;
+
+
 /**
  * This is a prototype for an initial view
  */
@@ -17,16 +25,43 @@ var InsightView = React.createClass({
      //var data =//this.props.result;
      var chartData = this.props.qValues;
 
-     var margins = {left: 100, right: 100, top: 50, bottom: 50};
-     var title = "Query" + this.props.qSeq;
+     var margins = {left: 35, right: 35, top: 35, bottom: 35};
+     var title = this.props.qSeq != null ? "Query" + this.props.qSeq : "No Query Chosen";
 
       // chart series,
       // field: is what field your data want to be selected
       // name: the name of the field that display in legend
       // color: what color is the line
 
+      // your x accessor
+      var x = function(d) {
+       return d.index;
+      }
+
+      var chartSeries = [
+      {
+         field: 'value',
+         color: '#ff7f0e',
+         legend: false,
+         style: {
+           "strokeWidth": 2,
+           "strokeOpacity": 1,
+           "fillOpacity": 0.5
+         }
+        }
+      ];
+
+    var legendJSX = <Legend
+        width= {this.props.width}
+        chartSeries = {chartSeries}
+        swatchShape= 'circle'
+        legendPosition = 'right'
+        offset = '200'
+      />;
 
      var d3JSX = <LineChart
+       margins={margins}
+       title={title}
        width= {this.props.width}
        height= {this.props.height}
        data= {chartData}
@@ -34,25 +69,9 @@ var InsightView = React.createClass({
        x= {x}
      />;
 
-     return <div className="containerD3"> {d3JSX} </div>
+
+     return <div className="containerD3">{d3JSX}</div>
    }
 });
-
-// your x accessor
-var x = function(d) {
- return d.index;
-}
-
-var chartSeries = [
-{
-   field: 'value',
-   color: '#ff7f0e',
-   style: {
-     "strokeWidth": 2,
-     "strokeOpacity": 1,
-     "fillOpacity": 0.5
-   }
-  }
-];
 
 module.exports = InsightView;
