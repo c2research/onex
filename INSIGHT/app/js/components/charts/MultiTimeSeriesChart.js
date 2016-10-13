82fbe0ddb41f4c2ccb5e35d0b19429c1485b4c45
Cuong Nguyen <nguyentricuong1995@gmail.com>
2016-10-13 13:41:17 +0700

    Made the chart resizable
    This is done by rerender the whole chart when there is changed in
    non-data props

--- INSIGHT/app/js/components/charts/MultiTimeSeriesChart.js
+++ INSIGHT/app/js/components/charts/MultiTimeSeriesChart.js
@@ -10,20 +10,46 @@ var MultiTimeSeriesChart = React.createClass({
     data: React.PropTypes.object
   },
 
+  // TODO(Cuong): consider this to be a mixin so that it can be reused in other charts
+  _detectChangeAndUpdateNonDataProps: function(newNonDataProps) {
+    var changed = false;
+    for (var field in newNonDataProps) {
+      if (this.nonDataProps[field] !== newNonDataProps[field]) {
+        changed = true;
+        break;
+      }
+    }
+    this.nonDataProps = newNonDataProps;
+    return changed;
+  },
+
   componentDidMount: function() {
     var el = ReactDOM.findDOMNode(this);
     this.d3MultiTimeSeriesChart = new D3MultiTimeSeriesChart();
-    this.d3MultiTimeSeriesChart.create(el, {
+    this.nonDataProps = {
       width: this.props.width,
       height: this.props.height,
       margins: this.props.margins,
       strokeWidth: this.props.strokeWidth
-    }, this.props.data);
+    }
+    this.d3MultiTimeSeriesChart.create(el, this.nonDataProps, this.props.data);
   },
 
   componentDidUpdate: function() {
     var el = ReactDOM.findDOMNode(this);
-    this.d3MultiTimeSeriesChart.update(el, this.props.data);
+    var nonDataProps = {
+      width: this.props.width,
+      height: this.props.height,
+      margins: this.props.margins,
+      strokeWidth: this.props.strokeWidth
+    };
+    if (this._detectChangeAndUpdateNonDataProps(nonDataProps)) {
+      this.d3MultiTimeSeriesChart.destroy(el);
+      this.d3MultiTimeSeriesChart.create(el, this.nonDataProps, this.props.data);
+    }
+    else {
+      this.d3MultiTimeSeriesChart.update(el, this.props.data);
+    }
   },
 
   componentWillUnmount: function() {
