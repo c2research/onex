a7e53bb311a41084d7c738cb63716734b012d9d0
Cuong Nguyen <nguyentricuong1995@gmail.com>
2016-11-30 12:53:11 -0500

    Disabled caching for easier testing

--- INSIGHT/server/routes.py
+++ INSIGHT/server/routes.py
@@ -36,6 +36,15 @@ def test():
   return render_template('test.html')
 
 
+@app.after_request
+def add_header(r):
+  r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+  r.headers["Pragma"] = "no-cache"
+  r.headers["Expires"] = "0"
+  r.headers['Cache-Control'] = 'public, max-age=0'
+  return r
+
+
 @app.route('/')
 def index():
   return render_template('layout.html')
